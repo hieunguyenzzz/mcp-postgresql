119c41ccf0b9a02dbd67d42eee0abc068aaa4852
Hieu Nguyen <hieunguyenel@gmail.com>
2025-03-15 22:15:17 +0700

    Update default port in run.py from 5000 to 5001

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = create_app()
 
 if __name__ == '__main__':
     # Get port from environment or use default
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     
     # Run the application
     app.run(host='0.0.0.0', port=port) 
